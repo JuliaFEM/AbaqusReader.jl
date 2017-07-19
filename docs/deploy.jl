ed389a9cd5f10d4f02ab0b9abd7ab5e6cb987cdf
Jukka Aho <jukka.aho@kapsi.fi>
2017-07-19 20:04:53 +0300

    Tune deploy scripts to upload documentation

--- docs/deploy.jl
+++ docs/deploy.jl
@@ -6,4 +6,5 @@ using AbaqusReader
 
 deploydocs(
     deps = Deps.pip("mkdocs", "python-markdown-math"),
-    repo = "github.com/JuliaFEM/AbaqusReader.jl.git")
+    repo = "github.com/JuliaFEM/AbaqusReader.jl.git",
+    julia = "0.6")
