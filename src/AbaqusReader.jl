918c34aeb562229a729105daf815d47925714c77
Jukka Aho <ahojukka5@gmail.com>
2018-03-11 17:15:43 +0700

    Implement new function abaqus_download (#29)
    Function can be used to download test models from internet. In order to
    use function, one must set environment variable ABAQUS_DOWNLOAD_URL.

--- src/AbaqusReader.jl
+++ src/AbaqusReader.jl
@@ -9,7 +9,9 @@ include("parse_mesh.jl")
 include("keyword_register.jl")
 include("parse_model.jl")
 include("create_surface_elements.jl")
+include("abaqus_download.jl")
 
 export abaqus_read_mesh, abaqus_read_model, create_surface_elements
+export abaqus_download
 
 end
