5399b61ed8e1b5fbf25bc2b4508ebfb3f4637ca5
Jukka Aho <ahojukka5@gmail.com>
2018-03-11 17:45:55 +0200

    Improve AbaqusReader.jl documentation
    Use the internal renderer of Documenter.jl

---
+++ docs/deploy.jl
@@ -0,0 +1,11 @@
+# This file is a part of JuliaFEM.
+# License is MIT: see https://github.com/JuliaFEM/MortarContact2D.jl/blob/master/LICENSE
+
+using Documenter
+
+deploydocs(
+    repo = "github.com/JuliaFEM/AbaqusReader.jl.git",
+    julia = "0.6",
+    target = "build",
+    deps = nothing,
+    make = nothing)
