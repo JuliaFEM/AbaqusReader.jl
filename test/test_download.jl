43bf045df9547cb9c096d514010957e301723ad4
Jukka Aho <ahojukka5@gmail.com>
2018-03-11 20:29:29 +0700

    Fix test for abaqus_download (#30)
    Return environment variables to the original state after finish test.

--- test/test_download.jl
+++ test/test_download.jl
@@ -5,19 +5,17 @@ using AbaqusReader
 using Base.Test
 
 @testset "test abaqus_download" begin
+    original_ENV = copy(ENV)
     delete!(ENV, "ABAQUS_DOWNLOAD_URL")
     delete!(ENV, "ABAQUS_DOWNLOAD_DIR")
     fn = tempname()
     touch(fn)
     model_name = basename(fn)
     ENV["ABAQUS_DOWNLOAD_DIR"] = dirname(fn)
-    println("fn = ", fn)
-    println("isfile(fn) = ", isfile(fn))
-    println("model_name = ", model_name)
-    println("ABAQUS_DOWNLOAD_DIR = ", ENV["ABAQUS_DOWNLOAD_DIR"])
     @test abaqus_download(model_name) == fn
-    rm(fn)
+    isfile(fn) && rm(fn)
     @test abaqus_download(model_name) == nothing
     ENV["ABAQUS_DOWNLOAD_URL"] = "https://models.com"
     @test abaqus_download(model_name; dryrun=true) == fn
+    merge!(ENV, original_ENV)
 end
