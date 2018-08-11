852392bf768e8d941d3a8516d9ac27dcd4a8117e
Jukka Aho <ahojukka5@gmail.com>
2018-08-11 13:07:20 +0300

    Make AbaqusReader 1.0 ready (#41)

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,10 +4,14 @@
 using AbaqusReader
 using Test
 
+include("../docs/make.jl")
+
 @testset "AbaqusReader.jl" begin
-    include("test_parse_mesh.jl")
-    include("test_parse_model.jl")
-    include("test_create_surface_elements.jl")
-    include("test_download.jl")
-    include("test_parse_t2d2.jl")
+    @testset "test_parse_mesh" begin include("test_parse_mesh.jl") end
+    @testset "test_parse_model" begin include("test_parse_model.jl") end
+    @testset "test_create_surface_elements" begin include("test_create_surface_elements.jl") end
+    @testset "test_download" begin include("test_download.jl") end
+    @testset "test_parse_t2d2" begin include("test_parse_t2d2.jl") end
 end
+
+include("../docs/deploy.jl")
