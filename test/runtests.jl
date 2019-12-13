a8d851d624c76393c91cd550d302854b6c5d4253
Jukka Aho <ahojukka5@gmail.com>
2019-12-13 13:57:59 +0700

    Add Project.toml file and remove REQUIRE

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,6 @@
 using AbaqusReader
 using Test
 
-include("../docs/make.jl")
-
 @testset "AbaqusReader.jl" begin
     @testset "test_parse_mesh" begin include("test_parse_mesh.jl") end
     @testset "test_parse_model" begin include("test_parse_model.jl") end
@@ -14,5 +12,3 @@ include("../docs/make.jl")
     @testset "test_parse_t2d2" begin include("test_parse_t2d2.jl") end
     @testset "test_parse_beams" begin include("test_parse_beams.jl") end
 end
-
-include("../docs/deploy.jl")
