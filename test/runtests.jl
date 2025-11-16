36d892ec225f151fdbd2162abcd9127bcbdb3444
Jukka Aho <ahojukka5@gmail.com>
2025-11-16 18:01:52 +0200

    Update runtests.jl to include new test files

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,10 +5,28 @@ using AbaqusReader
 using Test
 
 @testset "AbaqusReader.jl" begin
-    @testset "test_parse_mesh" begin include("test_parse_mesh.jl") end
-    @testset "test_parse_model" begin include("test_parse_model.jl") end
-    @testset "test_create_surface_elements" begin include("test_create_surface_elements.jl") end
-    @testset "test_download" begin include("test_download.jl") end
-    @testset "test_parse_t2d2" begin include("test_parse_t2d2.jl") end
-    @testset "test_parse_beams" begin include("test_parse_beams.jl") end
+    @testset "test_element_database" begin
+        include("test_element_database.jl")
+    end
+    @testset "test_parse_mesh" begin
+        include("test_parse_mesh.jl")
+    end
+    @testset "test_parse_model" begin
+        include("test_parse_model.jl")
+    end
+    @testset "test_model_sections" begin
+        include("test_model_sections.jl")
+    end
+    @testset "test_create_surface_elements" begin
+        include("test_create_surface_elements.jl")
+    end
+    @testset "test_download" begin
+        include("test_download.jl")
+    end
+    @testset "test_parse_t2d2" begin
+        include("test_parse_t2d2.jl")
+    end
+    @testset "test_parse_beams" begin
+        include("test_parse_beams.jl")
+    end
 end
