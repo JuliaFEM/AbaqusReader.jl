e8495219add1e857c3e1e4ec546b1858c5badc66
Jukka Aho <ahojukka5@gmail.com>
2025-11-16 18:04:10 +0200

    Update test_keyword_register.jl (kept for reference but not included in tests)

--- test/test_keyword_register.jl
+++ test/test_keyword_register.jl
@@ -1,12 +1,16 @@
 # This file is a part of JuliaFEM.
 # License is MIT: see https://github.com/JuliaFEM/AbaqusReader.jl/blob/master/LICENSE
 
-using Base.Test
-
-using AbaqusReader: register_abaqus_keyword, is_abaqus_keyword_registered
+using Test
 
 @testset "keyword register" begin
-    register_abaqus_keyword("HEI")
-    @test is_abaqus_keyword_registered("HEI")
-    @test !is_abaqus_keyword_registered("EI")
+    # Test using full module paths
+    AbaqusReader.register_abaqus_keyword("HEI")
+    @test AbaqusReader.is_abaqus_keyword_registered("HEI")
+    @test !AbaqusReader.is_abaqus_keyword_registered("EI")
+
+    # Test Val return type
+    val_result = AbaqusReader.register_abaqus_keyword("TEST_KEYWORD")
+    @test val_result == Val{:TEST_KEYWORD}
+    @test AbaqusReader.is_abaqus_keyword_registered("TEST_KEYWORD")
 end
